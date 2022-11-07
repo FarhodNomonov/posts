11027c855b2c711c6f002aa0322e8be93da9795b
100798f <farhodbek.nomonov@mail.ru>
2022-11-07 11:55:49 +0500

    update Andijan.js, Namangan.js and lang.json

--- components/AboutUzbPage/Cities/Andijan.js
+++ components/AboutUzbPage/Cities/Andijan.js
@@ -1,70 +1,135 @@
-import React from 'react'
-import Image from 'next/image';
-import Footer from "../../../components/Footer"
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Footer from "../../../components/Footer";
+import { getLanguage } from "../../../redux/selector";
+import Link from "next/link";
 
 const Andijan = () => {
-    return (
-        <>
-            <div className='absolute top-0 w-full h-[55vh] flex justify-center items-center'>
-                <Image src="https://www.advantour.com/img/uzbekistan/images/andijan.jpg" layout='fill' objectFit='cover' alt="img" />
-                <h1 className="text-white font-beyond text-[40px] xs:text-[70px] sm:text-[100px] leading-[1.5] pt-12 absolute inset-0 flex justify-center items-center bg-lightPurple">Andijan</h1>
+  const { language } = getLanguage();
+  return (
+    <>
+      <div className="absolute top-0 w-full h-[55vh] flex justify-center items-center">
+        <Image
+          src="https://www.advantour.com/img/uzbekistan/images/andijan.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="img"
+        />
+        <h1 className="text-white font-beyond text-[40px] xs:text-[70px] sm:text-[100px] leading-[1.5] pt-12 absolute inset-0 flex justify-center items-center bg-lightPurple">
+          {language["andijan"]}
+        </h1>
+      </div>
+
+      <div className="main-div grid grid-cols-4 py-[50px] mt-[40vh]">
+        <div className="col-span-3 space-y-4">
+          <h1 className="text-orange">
+            {language["andijan"]}, {language["uzbekistan"]}
+          </h1>
+          <h3 className="text-black">{language["andijan_about_one"]}</h3>
+          <p>
+            {language["andijan_about_two"]}
+          </p>
+
+          <p>
+            Historical records say about the large city on this site, which has
+            grown up on the track of the Silk Road. It served as the eastern
+            gateway of the Fergana Valley. On the whole route along the river of
+            Karadarya there were caravanserais.
+          </p>
+
+          <p>
+            For the first time Andijan is mentioned in records of the 9th
+            century. In the 9-10th centuries Andijan became a part of the
+            Samanid State. In the 14th century, during the reign of Amir Timur,
+            Andijan experienced rapid growth in all areas.
+          </p>
+
+          <p>
+            In the 16th century, Andijan was conquered by the Sheibanids, later
+            it became a part of the Kokand Khanate. Since 1876, the territory of
+            the Kokand Khanate, including Andijan, was included in the Russian
+            Empire. At this time, the city produced a wonderful silk and cotton
+            fabrics, which were famous not only in the Ferghana Valley, but far
+            beyond its borders. In 1902 Andijan suffered from an earthquake,
+            which destroyed a lot of ancient monuments.
+          </p>
+
+          <p>
+            The only main architectural monument - surviving a catastrophic
+            earthquake in 1902 is the huge Jami Madrasah. Built in the late 19th
+            century, this building is very impressive. Its length is almost 123
+            m. The complex occupies the area of 1.5 hectares, and includes a
+            madrasah, mosque and minaret.
+          </p>
+
+          <p>
+            Modern Andijan is the large city of Uzbekistan, the largest center
+            for engineering industry. Andijan has the automobile production
+            plant, a number of other major companies: engineering, canning and
+            dairy factories, flour mill, cotton mill, textile factory.
+          </p>
+
+          <p>
+            This beautiful sunny city becomes more attractive from year to year:
+            new modern buildings are built, range of entertainment and shopping
+            centers is growing. Andijan abounds in green gardens, parks and
+            alleys.
+          </p>
+
+          <p>
+            Thirty kilometers away from Andijan there is the ancient settlement
+            Ershi - the capital of the ancient state of Davan. Andijan region is
+            well-known for its holy places. So-called Teshik-Tash boulder
+            reminds straddling giant at the foot. Among other famous shrines
+            Imam-Ota, Tuzlik Masar, Ok Gur, spring Shirmanbulak should be
+            mentioned.
+          </p>
+
+          <h3 className="text-black">Andijan Pictures</h3>
+
+          <div className="grid grid-cols-3 gap-[30px]">
+            <div className="relative aspect-video">
+              <Image
+                src="https://www.advantour.com/img/uzbekistan/images/andijan.jpg"
+                layout="fill"
+                objectFit="cover"
+                alt="img"
+              />
             </div>
+            <div className="relative aspect-video">
+              <Image
+                src="https://www.advantour.com/img/uzbekistan/andijan/jami-complex_sm.jpg"
+                layout="fill"
+                objectFit="cover"
+                alt="img"
+              />
+            </div>
+            <div className="relative aspect-video">
+              <Image
+                src="https://www.advantour.com/img/uzbekistan/andijan/babur-park2_sm.jpg"
+                layout="fill"
+                objectFit="cover"
+                alt="img"
+              />
+            </div>
+          </div>
 
-            <div className='main-div grid grid-cols-4 py-[50px] mt-[40vh]'>
-
-                <div className='col-span-3 space-y-4'>
-                    <h1 className='text-orange'>Andijan, Uzbekistan</h1>
-                    <h3 className='text-black'>Tours, Attractions and Things To Do in Andijan</h3>
-                    <p>Ancient Andijan, surrounded by picturesque hills, is situated on the south-east of the Fergana Valley. This city is proud of its venerable age (2000 years) and of the fact that it is the hometown of Muhammad Zahiruddin Babur - the famous poet and commander, representative of the great dynasty of Tamerlane, the founder of the Mogul Empire in India. Today tourists can visit the house of Babur, remained in Andijan.</p>
-
-                    <p>Historical records say about the large city on this site, which has grown up on the track of the Silk Road. It served as the eastern gateway of the Fergana Valley. On the whole route along the river of Karadarya there were caravanserais.</p>
-
-                    <p>For the first time Andijan is mentioned in records of the 9th century. In the 9-10th centuries Andijan became a part of the Samanid State. In the 14th century, during the reign of Amir Timur, Andijan experienced rapid growth in all areas.</p>
-
-                    <p>In the 16th century, Andijan was conquered by the Sheibanids, later it became a part of the Kokand Khanate. Since 1876, the territory of the Kokand Khanate, including Andijan, was included in the Russian Empire. At this time, the city produced a wonderful silk and cotton fabrics, which were famous not only in the Ferghana Valley, but far beyond its borders. In 1902 Andijan suffered from an earthquake, which destroyed a lot of ancient monuments.</p>
-
-                    <p>The only main architectural monument - surviving a catastrophic earthquake in 1902 is the huge Jami Madrasah. Built in the late 19th century, this building is very impressive. Its length is almost 123 m. The complex occupies the area of 1.5 hectares, and includes a madrasah, mosque and minaret.</p>
-
-                    <p>Modern Andijan is the large city of Uzbekistan, the largest center for engineering industry. Andijan has the automobile production plant, a number of other major companies: engineering, canning and dairy factories, flour mill, cotton mill, textile factory.</p>
-
-                    <p>This beautiful sunny city becomes more attractive from year to year: new modern buildings are built, range of entertainment and shopping centers is growing. Andijan abounds in green gardens, parks and alleys.</p>
-
-                    <p>Thirty kilometers away from Andijan there is the ancient settlement Ershi - the capital of the ancient state of Davan. Andijan region is well-known for its holy places. So-called Teshik-Tash boulder reminds straddling giant at the foot. Among other famous shrines Imam-Ota, Tuzlik Masar, Ok Gur, spring Shirmanbulak should be mentioned.</p>
-
-                <h3 className='text-black'>Andijan Pictures</h3>
-
-                    <div className='grid grid-cols-3 gap-[30px]'>
-                        <div className='relative aspect-video'>
-                            <Image src="https://www.advantour.com/img/uzbekistan/images/andijan.jpg" layout='fill' objectFit='cover' alt="img" />
-                        </div>
-                        <div className='relative aspect-video'>
-                            <Image src="https://www.advantour.com/img/uzbekistan/andijan/jami-complex_sm.jpg" layout='fill' objectFit='cover' alt="img" />
-                        </div>
-                        <div className='relative aspect-video'>
-                            <Image src="https://www.advantour.com/img/uzbekistan/andijan/babur-park2_sm.jpg" layout='fill' objectFit='cover' alt="img" />
-                        </div>
-                    </div>
-
-                    {/* <h3 className='text-black'>Andrijan History</h3>
+          {/* <h3 className='text-black'>Andrijan History</h3>
                     <p>Ancient Andijan, surrounded by picturesque hills, is situated on the south-east of the Fergana Valley. This city is proud of its venerable age (2000 years) and of the fact that it is the hometown of Muhammad Zahiruddin Babur - the famous poet and commander, representative of the great dynasty of Tamerlane, the founder of the Mogul Empire in India. Today tourists can visit the house of Babur, remained in Andijan.</p>
                     <p>History of Samarkand goes back in remote days. Archeological finds and chronicle records of eyewitnesses and ancient historians allowed to establish with full reliability that a man lived on the territory of modern city many centuries before the Common Era.</p> */}
+        </div>
+        <div className="border border-gray-300 p-4 rounded-md h-max">
+          <Link href="/about-uzbekistan/andijan/tours" className="border">
+            <a className="text-black hover:text-red-500 transition-all cursor-pointer">
+              Andijan Tours
+            </a>
+          </Link>
+        </div>
+      </div>
 
-                </div>
-                <div className='border border-gray-300 p-4 rounded-md h-max'>
-                    <Link href="/about-uzbekistan/andijan/tours" className='border'>
-                        <a className='text-black hover:text-red-500 transition-all cursor-pointer'>Andijan Tours</a>
-                    </Link>
-                </div>
+      <Footer />
+    </>
+  );
+};
 
-            </div>
-
-            <Footer />
-
-
-        </>
-    )
-}
-
-
-export default Andijan
+export default Andijan;
--- components/AboutUzbPage/Cities/Namangan.js
+++ components/AboutUzbPage/Cities/Namangan.js
@@ -2,8 +2,10 @@ import React from "react";
 import Image from "next/image";
 import Footer from "../../../components/Footer";
 import Link from "next/link";
+import { getLanguage } from "../../../redux/selector";
 
 const Namangan = () => {
+  const { language } = getLanguage();
   return (
     <>
       <div className="absolute top-0 w-full h-[55vh] flex justify-center items-center">
@@ -14,50 +16,20 @@ const Namangan = () => {
           alt="img"
         />
         <h1 className="text-white font-beyond text-[40px] xs:text-[70px] sm:text-[100px] leading-[1.5] pt-12 absolute inset-0 flex justify-center items-center bg-lightPurple">
-          Namangan
+          {language["namangan"]}
         </h1>
       </div>
       z
       <div className="main-div grid grid-cols-4 py-[50px] mt-[40vh]">
         <div className="col-span-3 space-y-4">
-          <h1 className="text-orange">Namangan, Uzbekistan</h1>
-          <h3 className="text-black">
-            Tours, Attractions and Things To Do in Namangan
-          </h3>
-
-          <p>
-            Namangan city dates back to the 17th century, when, in the north of
-            the Ferghana Valley, a settlement for local salt miners of ores
-            (naman kann) has appeared. In the 17th century after the devastating
-            earthquake residents of Aksikent (Ahsykenta) moved here. Aksikent is
-            the ancient city, which once was the center of the Fergana Valley.
-            In the middle of the 18th century the city became the administrative
-            center of the province (viloyat).
-          </p>
-
-          <p>
-            In 1875, Namangan became the part of Russia. At that time the new
-            city was laid under the regular plan. New city was separated from
-            the old part by the fortress, from which, according to established
-            order, streets were laid in a semicircle. In the early twentieth
-            century, Namangan was the second most populous city and center of
-            cotton processing in the Fergana Valley, and then the third largest
-            in the country, after Tashkent and Samarkand.
-          </p>
-
-          <p>
-            Currently, Namangan is developed as the center of light and food
-            industries. The city abounds in green orchards and parks. Not far
-            from Namangan there are ruins of the ancient city of Aksikent.
-            Archaeological researches state that the city had a citadel and
-            strong fortifications. Trade and handicraft production was
-            well-developed here. Before the thirteenth century Aksikent was the
-            capital of the Fergana Valley. But then it was destroyed by the
-            Mongols, and in the seventeenth century, the strongest earthquake
-            destroyed it to the ground.
-          </p>
-
-          <h3 className="text-black">Namangan Pictures</h3>
+          <h1 className="text-orange">
+            {language["namangan"]}, {language["uzbekistan"]}
+          </h1>
+          <h3 className="text-black">{language["namangan_about"]}</h3>
+          <p>{language["namangan_about_one"]}</p>
+          <p>{language["namangan_about_two"]}</p>
+          <p>{language["namangan_about_three"]}</p>
+          <h3 className="text-black">{language["namangan_pct"]}</h3>
 
           <div className="grid grid-cols-3 gap-[30px]">
             <div className="relative aspect-video">
@@ -87,13 +59,13 @@ const Namangan = () => {
           </div>
         </div>
 
-        <div className="border border-gray-300 p-4 rounded-md h-max">
+        {/* <div className="border border-gray-300 p-4 rounded-md h-max">
           <Link href="/about-uzbekistan/fergana/tours" className="border">
             <a className="text-black hover:text-red-500 transition-all cursor-pointer">
               Namangan Tours
             </a>
           </Link>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </>
