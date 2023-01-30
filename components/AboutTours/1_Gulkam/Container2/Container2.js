4b8d0536b808e36cfc0bea044f1231fbd46c0808
NomonovFarhod <farhodbek.nomonov@mail.ru>
2023-01-30 13:24:43 +0500

    update Container2.js, Swiper.js and lang.json

--- components/AboutTours/1_Gulkam/Container2/Container2.js
+++ components/AboutTours/1_Gulkam/Container2/Container2.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { nameLang, descriptionLang } from "../../../../utils/func";
 import { getLanguage } from "../../../../redux/selector";
 import { AccordionTop } from "../../../../public/icons";
+// import { useMemo } from "react";
 
 const Container2 = () => {
   const router = useRouter();
@@ -15,6 +16,17 @@ const Container2 = () => {
   const { language } = getLanguage();
   const src = `https://tours.techdatasoft.uz/cover/${tours?.cover}`;
   const [openAccordion, setOpenAccordion] = React.useState(null);
+  // const [openTab, setOpenTab] = React.useState(0);
+
+  // const tabs = useMemo(() => {
+  //   const Photo = () => (
+     
+  //   );
+  //   const Daria = () => <div>Daria</div>;
+
+  //   const data = [<Photo />, <Daria />];
+  //   return data[openTab];
+  // }, [openTab, openAccordion, language]);
 
   const accordion_data = [
     {
@@ -62,92 +74,126 @@ const Container2 = () => {
   };
 
   return (
-    <div className="main-div grid grid-cols-2 ml:grid-cols-1 py-[100px]  gap-[20px]">
+    <div className="main-div grid grid-cols-1 ml:grid-cols-1 py-[100px] ml:py-[20px] gap-[20px]">
       {/* left */}
-      <div className="col-span-2 space-y-12 ">
-        <h1 className="ml:text-[32px]">{nameLang(tours, language)}</h1>
 
-        <div className="flex flex-col gap-2 ml:w-full">
-          <div className="w-full aspect-video relative">
-            <Image
-              src={src}
-              layout="fill"
-              objectFit="cover"
-              alt="img"
-              unoptimized
-            />
-            <div className="bg-orange text-white absolute top-8 left-8 py-2 px-8 flex flex-col items-center rounded-full ml:top-2 ml:left-2 ml:w-[100px]">
-              {/* <span className='text-[24px] font-bold'>01</span> */}
-              <span className="text-[24px] font-bold ml:text-[10px]">
-                {new Date(tours?.created_at)?.toLocaleDateString()}
-              </span>
+      <div className="main-div grid grid-cols-1 ml:grid-cols-1 py-[100px] ml:py-[20px] gap-[20px]">
+        {" "}
+        <div className="col-span-2 space-y-12 ">
+          <h1 className="ml:text-[20px]">{nameLang(tours, language)}</h1>
+
+          <div className="flex flex-col gap-2 ml:w-full">
+            <div className="w-full aspect-video relative">
+              <Image
+                src={src}
+                layout="fill"
+                objectFit="cover"
+                alt="img"
+                unoptimized
+              />
+              <div className="bg-orange text-white absolute top-8 left-8 py-2 px-8 flex flex-col items-center rounded-full ml:top-2 ml:left-2 ml:w-[100px]">
+                {/* <span className='text-[24px] font-bold'>01</span> */}
+                <span className="text-[24px] font-bold ml:text-[10px]">
+                  {new Date(tours?.created_at)?.toLocaleDateString()}
+                </span>
+              </div>
             </div>
-          </div>
-          <div>
-            <h3 className="text-black font-semibold">
-              {nameLang(tours, language)}
-            </h3>
-            <div className="accordion rounded border-inherit select-none">
-              {accordion_data?.map((item, index) => (
-                <React.Fragment key={index}>
-                  <div className="mx-auto ">
-                    <div
-                      onClick={() => close(index)}
-                      className="mt-1  cursor-pointer  py-2 px-[10px] text-white flex justify-between items-center rounded border-inherit border-solid border transition-all delay-500"
-                    >
-                      <h3 className="text-base text-gray-600 text-left">
-                        {item?.title}
-                      </h3>
-                      <span className="title_scg ">{item?.icon}</span>
+            <div>
+              <h3 className="text-black font-semibold ml:text-[20px]">
+                {nameLang(tours, language)}
+              </h3>
+              <div className="accordion rounded border-inherit select-none">
+                {accordion_data?.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <div className="mx-auto ">
+                      <div
+                        onClick={() => close(index)}
+                        className="mt-1  cursor-pointer  py-2 px-[10px] text-white flex justify-between items-center rounded border-inherit border-solid border transition-all delay-500"
+                      >
+                        <h3 className="text-base text-gray-600 text-left">
+                          {item?.title}
+                        </h3>
+                        <span className="title_scg ">{item?.icon}</span>
+                      </div>
+                      {openAccordion === index ? (
+                        <p className="text-[#2B2B2B] px-5 py-3 ">
+                          {item?.description}
+                        </p>
+                      ) : null}
                     </div>
-                    {openAccordion === index ? (
-                      <p className="text-[#2B2B2B] px-5 py-3 ">
-                        {item?.description}
-                      </p>
-                    ) : null}
-                  </div>
-                </React.Fragment>
-              ))}
-            </div>
-            <p className="leading-[2.14] mt-6 mb-8">
-              {descriptionLang(tours, language)}
-            </p>
-            <div className="prices">
-              <h3 className="text-orange font-semibold mb-2">Prices</h3>
-              <div className="price_card flex bg-slate-100 p-2 rounded text-black justify-between mb-3">
-                <p>1 {language["person"]}</p>
-                <a>${tours?.price}</a>
+                  </React.Fragment>
+                ))}
               </div>
-              <div className="price_card flex bg-slate-100 p-2 rounded text-black justify-between mb-3">
-                <p>2 {language["person"]}</p>
-                <a>${(tours?.price * 60) / 100}</a>
+              <p className="leading-[2.14] mt-6 mb-8">
+                {descriptionLang(tours, language)}
+              </p>
+              <div className="prices">
+                <h3 className="text-orange font-semibold mb-2">Prices</h3>
+                <div className="price_card flex p-2 rounded text-black justify-between">
+                  <p className="ml-[40px]"></p>
+                  <p>Econ</p>
+                  <p>Comf</p>
+                </div>
+                <div className="price_card flex bg-slate-100 p-2 rounded text-black justify-between mb-3">
+                  <p>1 {language["person"]}</p>
+                  <p>${(tours?.price * 60) / 100}</p>
+                  <a>${tours?.price}</a>
+                </div>
+                <div className="price_card flex bg-slate-100 p-2 rounded text-black justify-between mb-3">
+                  <p>2 {language["person"]}</p>
+                  <p>${(tours?.price * 50) / 100}</p>
+                  <a>${(tours?.price * 60) / 100}</a>
+                </div>
+                <div className="price_card flex bg-slate-100 p-2 rounded text-black justify-between mb-3">
+                  <p>3 {language["person"]}</p>
+                  <a>${(tours?.price * 50) / 100}</a>
+                  <a>${(tours?.price * 70) / 100}</a>
+                </div>
               </div>
-              <div className="price_card flex bg-slate-100 p-2 rounded text-black justify-between mb-3">
-                <p>3 {language["person"]}</p>
-                <a>${(tours?.price * 70) / 100}</a>
-              </div>
-            </div>
-            <div className="flex gap-5">
-              <form
-                method="POST"
-                action="https://checkout.paycom.uz"
-                target="_blank"
-              >
-                <input
-                  type="hidden"
-                  name="merchant"
-                  defaultValue="63c66eb72ef2d4e9e90c1f77"
-                />
-                <input
-                  type="hidden"
-                  name="amount"
-                  defaultValue={tours?.price * 11200 + "00"}
-                />
-                <input
-                  type="hidden"
-                  name="account[tour_name]"
-                  defaultValue="DUPPI TRAVEL"
-                />
+              <div className="flex gap-5">
+                <form
+                  method="POST"
+                  action="https://checkout.paycom.uz"
+                  target="_blank"
+                >
+                  <input
+                    type="hidden"
+                    name="merchant"
+                    defaultValue="63c66eb72ef2d4e9e90c1f77"
+                  />
+                  <input
+                    type="hidden"
+                    name="amount"
+                    defaultValue={tours?.price * 11200 + "00"}
+                  />
+                  <input
+                    type="hidden"
+                    name="account[tour_name]"
+                    defaultValue="DUPPI TRAVEL"
+                  />
+                  <button
+                    type="submit"
+                    className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ... font-[700] leading-[53px] px-[30px]"
+                    style={{
+                      cursor: "pointer",
+                      border: "1px solid #ebebeb",
+                      borderRadius: "6px",
+                      background: "linear-gradient(to top, #f1f2f2, white)",
+                      width: "200px",
+                      height: "42px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <img
+                      style={{ width: "160px", height: "20px" }}
+                      src="http://cdn.payme.uz/buttons/button_big_RU.svg"
+                    />
+                    <p className="p-2 text-black"> ${tours?.price} </p>
+                  </button>
+                </form>
+
                 <button
                   type="submit"
                   className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ... font-[700] leading-[53px] px-[30px]"
@@ -165,36 +211,33 @@ const Container2 = () => {
                 >
                   <img
                     style={{ width: "160px", height: "20px" }}
-                    src="http://cdn.payme.uz/buttons/button_big_RU.svg"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/1200px-Visa_Inc._logo.svg.png"
                   />
-                  <p className="p-2 text-black"> ${tours?.price} </p>
                 </button>
-              </form>
-
-              <button
-                type="submit"
-                className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ... font-[700] leading-[53px] px-[30px]"
-                style={{
-                  cursor: "pointer",
-                  border: "1px solid #ebebeb",
-                  borderRadius: "6px",
-                  background: "linear-gradient(to top, #f1f2f2, white)",
-                  width: "200px",
-                  height: "42px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  style={{ width: "160px", height: "20px" }}
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/1200px-Visa_Inc._logo.svg.png"
-                />
-              </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      {/* <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <ul className="flex space-x-2 cursor-pointer">
+            {[0, 1].map((key) => (
+              <li key={key}>
+                <button
+                  onClick={() => setOpenTab(key)}
+                  className="inline-block px-4 py-2 text-gray-600 bg-white rounded shadow"
+                >
+                  {key}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <div className="p-3 mt-6 bg-white border">{tabs}</div>
+        </div>
+      </div> */}
 
       {/* right */}
       {/* <div className="col-span-1 flex flex-col ml:ml-[10px] items-start ml-[50px]">
@@ -217,11 +260,11 @@ const Container2 = () => {
           );
         })}
       </div> */}
-      <div className="about_pay">
+      {/* <div className="about_pay">
         <button className="text-[14px] font-[700] text-white leading-[53px] px-[46px] relative overflow-hidden mt-[50px] sm:mt-[80px] rounded-[100vmax] button1">
           <div className="btn1-bg absolute inset-0 -z-[1]"></div>
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
--- components/HomePage/Container3/Swiper/Swiper.js
+++ components/HomePage/Container3/Swiper/Swiper.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 import { useSelector } from "react-redux";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -11,114 +11,72 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Navigation } from "swiper";
-import Image from "next/image";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-import { StarIcon } from "@heroicons/react/solid";
-import { nameLang } from "../../../../utils/func";
+// import { Autoplay, Navigation } from "swiper";
+// import Image from "next/image";
+// import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+// import { StarIcon } from "@heroicons/react/solid";
+import { nameLang, descriptionLang } from "../../../../utils/func";
 import { getLanguage } from "../../../../redux/selector/index";
 
 export default function SwiperHotelList() {
-  const categories = useSelector(({ categories }) => categories);
+  const tours = useSelector(({ tours }) => tours);
   const { language } = getLanguage();
 
-  const Retings = (res) => {
-    let copyItems = [];
-    for (let i = 0; i < res; i++) {
-      copyItems.push(i);
-    }
-    return copyItems;
-  };
+  // const Retings = (res) => {
+  //   let copyItems = [];
+  //   for (let i = 0; i < res; i++) {
+  //     copyItems.push(i);
+  //   }
+  //   return copyItems;
+  // };
 
   return (
-    <>
-      <Swiper
-        spaceBetween={0}
-        slidesPerView="auto"
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        navigation={{
-          nextEl: ".next",
-          prevEl: ".prev",
-        }}
-        speed={300}
-        breakpoints={{
-          280: {
-            slidesPerView: 1.3,
-            spaceBetween: 20,
-          },
-          520: {
-            slidesPerView: 1.8,
-            spaceBetween: 20,
-          },
-          640: {
-            slidesPerView: 2.5,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3.5,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 4.5,
-            spaceBetween: 20,
-          },
-        }}
-        loop={true}
-        modules={[Autoplay, Navigation]}
-        className="mySwiper2"
-      >
-        {categories?.map((res) => {
-          const src = `https://tours.techdatasoft.uz/cover/${res?.cover}`;
+    <div className="container_tours">
+      <h1 className="text-[30px] text-black text-center mb-2 uppercase">
+        {language["upcoming"]}{" "}
+        <span className="text-orange uppercase">{language["tours"]}</span>{" "}
+        {language["in"]} {new Date().getFullYear()}
+      </h1>
+      <div className="tours_status flex justify-end gap-[60px] mr-[40px] ml:hidden">
+        {/* <p className="text-base text-black font-normal">Status</p> */}
+        <p className="text-base text-black font-normal">Days</p>
+        <p className="text-base text-black font-normal">Price</p>
+      </div>
+      <div className="tours_box mt-[20px]">
+        {tours.map((res) => {
           return (
-            <SwiperSlide>
-              <div className="relative w-full max-w-[400px] aspect-[4/5]">
-                <Image
-                  unoptimized
-                  layout="fill"
-                  src={src}
-                  alt="Picture of the author"
-                />
-                <div className="absolute inset-0 bg-lightPurple px-4 lg:px-8 py-8 flex flex-col">
-                  <div className="px-[25px] text-[13px] text-white rounded-[14px] bg-orange leading-[27px] w-max mx-auto">
+            <Link href={`/tourInfo/${res?.id}`}>
+              <div className="tours_box_in w-full p-2 rounded-[54px] bg-[#350A4E] flex gap-[20px] justify-around items-center mb-[20px] ml:flex-col">
+                <div className="w-[20%] bg-[#FA9E1B] rounded-l-[54px] flex justify-center items-center text-center ml:w-[80%] ml:rounded-[4px] ml:p-[7px]">
+                  <h1 className="text-white text-[30px] font-black ml:text-[16px]">
                     {new Date(res?.updated_at).toDateString()}
-                  </div>
-                  <h1 className="text-[16px] lg:text-[16px] font-[700] text-white tracking-tighter text-center mt-auto">
-                    {nameLang(res, language)}
                   </h1>
-                  {/* <p className='text-white text-center font-[200] leading-3'>From: $1450</p> */}
-                  <div className="flex justify-center mt-[10px]">
-                    {Retings(res?.stars).map((i) => {
-                      return <StarIcon key={i} className="w-4 text-orange" />;
-                    })}
-                  </div>
-                  <div className="text-center">
-                    <button className="text-[13px] font-[700] text-white leading-[50px] lg:leading-[40px] px-[35px] lg:px-[40px] relative z-[1] overflow-hidden mt-[20px] lg:mt-[40px] rounded-[100vmax] button1">
-                      <div className="btn1-bg absolute inset-0"></div>
-                      <Link href={`/tourInfo/${res?.id}`}>
-                        <a span className="relative">
-                          {language["read"]}
-                        </a>
-                      </Link>
-                    </button>
-                  </div>
+                </div>
+                <div className="w-[45%] ml:w-[100%] ml:text-center">
+                  <h3 className="text-white text-[20px]">
+                    {nameLang(res, language)}
+                  </h3>
+                  <h3 className="text-white text-[14px] max-h-[70px] overflow-auto">
+                    {descriptionLang(res, language)}
+                  </h3>
+                </div>
+                {/* <div className="text-white">
+                <h4>AVAILABLE</h4>
+              </div> */}
+                <div className="text-white ml:flex ml:gap-[10px]">
+                  <h4 className="hidden ml:block">{language["days"]}:</h4>
+                  <h4>{res?.days}</h4>
+                </div>
+                <div className="text-white text-center">
+                  <h4>
+                    US$ <br className="ml:hidden" /> {res?.price}
+                  </h4>
                 </div>
               </div>
-            </SwiperSlide>
+            </Link>
           );
         })}
-      </Swiper>
-
-      <div className="md:flex justify-center gap-8 hidden mt-10">
-        <div className="prev hidden md:block group border-orange bg-slate-500 rounded-[9px] overflow-hidden border-2 py-[12px] px-[2px] cursor-pointer">
-          <MdKeyboardArrowLeft className="text-[20px] text-orange" />
-        </div>
-        <div className="next hidden md:block group border-orange bg-slate-500 border-2 py-[12px] px-[2px] rounded-[9px] cursor-pointer">
-          <MdKeyboardArrowRight className="text-[20px] text-orange" />
-        </div>
       </div>
-    </>
+    </div>
   );
 }
